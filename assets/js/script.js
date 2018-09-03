6d16f9cfad80c31e18efdb46194fe7d719558615
Lynn Thompson <lynninla@earthlink.net>
2018-09-03 11:44:45 -0700

    Added responsive nav bar hamburger icon

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+//     $(".home_race_image_link").hover(function(){
+//   		var thisSrc = $(this).attr("data-background");
+//         $(this).stop().animate({'bottom': '8px'}, 500);
+// 		$(".container").css({
+// 			"background-image": "url(" + thisSrc + ")",
+// 			"-webkit-transition": "background-image 0.7s ease-in-out",
+// 			"transition": "background-image 0.7s ease-in-out"
+// 		});
+			
+//     }, function(){
+//         $(this).stop().animate({'bottom': '0px'}, 500);
+//         $(".container").css({
+// 			"background-image": "url('assets/images/imperial_guard_war.jpg')",
+// 			"-webkit-transition": "background-image 0.7s ease-in-out",
+// 			"transition": "background-image 0.7s ease-in-out"
+// 		});
+//     });
+
+	// $('a[href^="#"]').on('click',function (e) {
+	//     e.preventDefault();
+
+	//     var target = this.hash;
+	//     var $target = $(target);
+
+	//     $('html, body').stop().animate({
+	//         'scrollTop': $target.offset().top
+	//     }, 900, 'swing', function () {
+	//         window.location.hash = target;
+	//     });
+	// });
+
+	$(".nav_link").click(function() {
+		$(".topnav").toggleClass("responsive");
+	})
+
+	$("#icon_link").click(function() {
+    	$(".topnav").toggleClass("responsive");
+	})
+
+});
